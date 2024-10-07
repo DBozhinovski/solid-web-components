265a762d4264125219f492de3cbd883b58a151e6
Darko Bozhinovski <dbozhinovski@gmail.com>
2024-10-07 17:05:12 +0200

    fix: removes unused props, this time for real

--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -1,17 +1,13 @@
 import { customElement } from "solid-element";
 
-customElement(
-  "my-component",
-  { message: "Hello, World!" },
-  (props, { element }) => {
-    return (
-      <div>
-        <h1>{props.message}</h1>
-        <p>This is a custom web component built with solid-element.</p>
-      </div>
-    );
-  }
-);
+customElement("my-component", { message: "Hello, World!" }, (props) => {
+  return (
+    <div>
+      <h1>{props.message}</h1>
+      <p>This is a custom web component built with solid-element.</p>
+    </div>
+  );
+});
 
 declare module "solid-js" {
   namespace JSX {
